9c7c597d34f70f93b2b7f69e0c3e8488eb84c615
Pelayo <garciapalaciospelayo@gmail.com>
2024-04-03 14:30:10 +0200

    Implement Contact Dowload
    Se implementa nueva funcionalidad de descarga de contacto. Con el fin de implementar un boton de descarga de contacto se modifican los archivos Eloy.htm , styles.css y Scripts.js implementando nuevo codigo

--- scripts.js
+++ scripts.js
@@ -36,3 +36,40 @@ document.querySelector('.qr-section .qr-link').addEventListener('click', functio
     });
     document.body.appendChild(modal);
 });
+
+//Funcion que se ejecutara cuando pulsemos el boton para descargar el contacto 
+function abrirContacto() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  //Verifica el sistema del terminal que se esta usando, si es android lo guardara en la lista de contactos  
+  if (/android/i.test(userAgent)) {
+    // Dispositivo Android
+    window.location.href = "intent:content://com.android.contacts/contacts#Intent;action=android.intent.action.VIEW;end";
+  }
+}
+
+//Funcion para mostrar o ocultar el popUP
+function mostrarOcultar() {
+  var panel = document.getElementById('aviso');
+  if (panel.style.display === "none") {
+    panel.style.display = "block";
+  } else {
+    panel.style.display = "none";
+  }
+  panel.style.borderRadius = '0';
+  panel.style.position = 'fixed';
+  panel.style.top = '0';
+  panel.style.left = '0';
+  panel.style.width = '100vw';
+  panel.style.height = '100vh';
+  panel.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+  panel.style.display = 'flex';
+  panel.style.justifyContent = 'center';
+  panel.style.alignItems = 'center';
+  panel.style.zIndex = '1000';
+}
+// Ocultar el div cuando se hace clic dentro de él
+document.getElementById('aviso').addEventListener('click', function (event) {
+  this.style.display = 'none';
+});
+
